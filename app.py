d34a00b4c1d11f56614b882e1dd51e9d218cc0c4
Eide Nonato <eide.trt@gmail.com>
2025-11-25 18:10:26 -0300

    Adiciona classificacao de peso

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-#Calculadora de IMC
-#Desenvolvido por: Eide Nonato
-
-print("Bem-vindo à Calculadora de Saúde")
+#Calculadora de IMC - Versão 2.0
+print("Bem-vindo à Calculadora de ´Saúde")
 
 peso = float(input("Digite seu peso (kg): "))
 altura = float(input("Digite sua altura (m): "))
 
-imc = peso / (altura ** 2)
-print("Seu IMC é:" imc)
+imc = peso/ (altura ** 2)
+
+print(f"Seu IMC é: {imc:.2f}")
+
+#Nova funcionalidade:
+Classificação
+if imc <18.5:
+    print("Classificação: Abaixo do peso")
+elif imc < 25:
+    print("Classificação: Peso normal")
+else:
+    print("Classificação: Sobrepeso ou Obesidade")
