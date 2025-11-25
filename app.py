9f3b5fe99400af5a5595f87d74141dadbca1530b
Eide Nonato <eide.trt@gmail.com>
2025-11-25 19:57:11 -0300

    Corrigindo erros de diigitacao e logica

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-#Calculadora de IMC - Versão 2.0
-print("Bem-vindo à Calculadora de ´Saúde")
+# Calculadora de IMC - Versão 2.0
+print("Bem-vindo à Calculadora de Saúde")
 
+# Correção 1: Fechamos os dois parênteses no final
 peso = float(input("Digite seu peso (kg): "))
 altura = float(input("Digite sua altura (m): "))
 
-imc = peso/ (altura ** 2)
+imc = peso / (altura ** 2)
 
+# Correção no formato do f-string (sem espaços dentro das chaves)
 print(f"Seu IMC é: {imc:.2f}")
 
-#Nova funcionalidade:
-Classificação
-if imc <18.5:
+# #Nova funcionalidade (comentar para não dar erro)
+# Classificação
+
+# Correção 2 e 3: Usar IF/ELIF/ELSE e corrigir 'inc' para 'imc'
+if imc < 18.5:
     print("Classificação: Abaixo do peso")
 elif imc < 25:
     print("Classificação: Peso normal")
